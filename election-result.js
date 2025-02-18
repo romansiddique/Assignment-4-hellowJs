0f356848972043392962b74dc88e430fd944d1b2
romansiddique <romansiddique66626@gmail.com>
2025-02-18 18:46:12 +0600

    election-result problem solve

--- election-result.js
+++ election-result.js
@@ -1,6 +1,27 @@
 function electionResult( votes ) {
-  for (partyName of votes){
-    
+  let countMango = 0;
+  let countBanana = 0;
+  if(!Array.isArray(votes)){
+    return "Invalid";
+  }
+  else{
+    for (const partyName of votes){
+      if(partyName === "mango"){
+        countMango +=1;
+      }
+      else if(partyName === "banana"){
+        countBanana +=1;
+      }
+    }
+    if(countMango > countBanana){
+      return "Mango";
+    }
+    else if(countBanana > countMango){
+      return "Banana";
+    }
+    else{
+      return "Draw"
+    }
   }
   }
-  console.log(electionResult(["mango", "bnana", "no-vote", "mango"]))
+  
