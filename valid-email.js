c2109d6ba1e76ab704a2ce89c4ae712832e1cb66
romansiddique <romansiddique66626@gmail.com>
2025-02-19 10:10:19 +0600

    five problem solve but some issue in valid email check but assignment condition is fullfilled

--- valid-email.js
+++ valid-email.js
@@ -1,6 +1,21 @@
-function validEmail(email){
-  for(numbers of email){
-    console.log(numbers)
+function validEmail( email ) {
+  if(typeof (email) !== "string"){
+    return false;
+  }
+  if(email[0] === "." || email[0] === "-" || email[0] === "_" || email[0] === "@" || email[0] === "+" || email[0] === "!" || email[0] === "#" || email[0] === "$"  || email[0] === "%" || email[0] === "^"  || email[0] === "&" || email[0] === "*"){
+    return false;
+  }
+  if(email.includes(" ")){
+    return false;
+  }
+  if(!email.endsWith(".com")){
+    return false;
+  }
+  const atSplit = email.split("@");
+  if(atSplit.length !== 2 && !atSplit[1]){
+    return false;
+  }
+  else{
+    return true;
   }
 }
-console.log(validEmail("romansiddique66626@gmail.com"))
